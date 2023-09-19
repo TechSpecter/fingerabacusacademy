b61e12fafef20cfa4a57f9c7e592d7c698557029
Rahul Gupta <mail2guptar@gmail.com>
2023-09-19 15:32:41 +0000

    Added Logo Stuff's

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,21 +1,10 @@
+<!-- <$vuetify.theme.dark> -->
 <template>
-  <svg width="151" height="55" viewBox="0 0 151 55" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path
-      d="M22 1H2A1 1 0 0 0 1 2V22a1 1 0 0 0 1 1H22a1 1 0 0 0 1-1V2A1 1 0 0 0 22 1ZM21 21H3V3H21V21ZM7 12a1 1 0 0 1 1-1h3V8a1 1 0 0 1 2 0v3h3a1 1 0 0 1 0 2H13v3a1 1 0 0 1-2 0V13H8A1 1 0 0 1 7 12ZM48 1H28a1 1 0 0 0-1 1V22a1 1 0 0 0 1 1H48a1 1 0 0 0 1-1V2A1 1 0 0 0 48 1ZM47 21H29V3H47V21ZM33 12a1 1 0 0 1 1-1h8a1 1 0 0 1 0 2H34A1 1 0 0 1 33 12ZM22 27H2a1 1 0 0 0-1 1V48a1 1 0 0 0 1 1H22a1 1 0 0 0 1-1V28A1 1 0 0 0 22 27ZM21 47H3V29H21V47ZM8.46 40.12L10.59 38 8.46 35.88a1 1 0 0 1 1.42-1.42L12 36.59l2.12-2.13a1 1 0 0 1 1.42 1.42L13.41 38l2.13 2.12a1 1 0 0 1 0 1.42 1 1 0 0 1-0.71 0.29 1 1 0 0 1-0.71-0.29L12 39.41 9.88 41.54a1 1 0 0 1-0.71 0.29 1 1 0 0 1-0.71-0.29A1 1 0 0 1 8.46 40.12ZM48 27H28a1 1 0 0 0-1 1V48a1 1 0 0 0 1 1H48a1 1 0 0 0 1-1V28A1 1 0 0 0 48 27ZM47 47H29V29H47V47ZM33 39a1 1 0 0 1 1-1h8a1 1 0 0 1 0 2H34A1 1 0 0 1 33 39Zm4-4a1 1 0 1 1 1 1A1 1 0 0 1 37 35Zm2 8a1 1 0 1 1-1-1A1 1 0 0 1 39 43Z" :class="$vuetify.theme.dark ? 'fill-light-color' : 'fill-dark-color'"></path>
-
-  </svg>
+<v-img :src="require('../static/logo/logo-no-background.svg')" height="55px" width="450px" class="padding:0px"></v-img>
 </template>
 
 <style scoped>
-.fill-theme-color {
-  fill: #1976d2;
-}
-
-.fill-light-color {
-  fill: #fff;
-}
-
-.fill-dark-color {
-  fill: #000;
-}
 </style>
+
+<script>
+</script>
